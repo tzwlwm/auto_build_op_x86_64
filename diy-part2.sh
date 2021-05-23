dc6b112ac4175cede531490e1559e43da3adf777
tzwlwm <tzwlwm@163.com>
2021-05-23 21:04:44 +0800

    更新node版本为16.2.0

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,6 @@
 sed -i 's#192.168.1.1#192.168.50.1#g' package/base-files/files/bin/config_generate
 # 替换默认主题
 sed -i 's#luci-theme-bootstrap#luci-theme-argon#g' feeds/luci/collections/luci/Makefile
+#更新node版本为16.2.0
+sed -i "s/^.*PKG_VERSION:.*$/PKG_VERSION:=v16.2.0/" package/feeds/packages/node/Makefile
+sed -i "s/^.*PKG_HASH:.*$/PKG_HASH:=d0f93b9842afb8f23c07862e9cd48226e7104547f7b2415d250fdb752d1b35cf/" package/feeds/packages/node/Makefile
